e01653cd51757d452ac3c6c6815815c4e8717415
yevhen <yevhen.krasnokutsky@gmail.com>
2022-09-10 13:35:21 +0300

    HW2 PR1 Resolve review comments

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM python:3.10-alpine3.16
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
-# RUN apt-get update -y && apt-get upgrade -y
 WORKDIR /app
 COPY requirements.txt requirements.txt
+RUN addgroup user && adduser -u 9999 -G user -D user
+USER user
+ENV PATH="${PATH}:/home/user/.local/bin/"
 RUN pip3 install --upgrade pip && pip3 install -r requirements.txt
 COPY . .
 EXPOSE 5000
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from model import DummyModel
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify
 import json
 
 
@@ -10,11 +10,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def process():
     if request.method == 'GET':
-        return Response(response=json.dumps({"response": "ok"}), status=200, mimetype="application/json")
+        return jsonify(response="ok")
     else:
         texts = request.json["texts"]
         response = model.process(texts)
-        return Response(response=json.dumps({"response": response}), status=200, mimetype="application/json")
+        return jsonify(response=response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
