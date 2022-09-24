ea07964bec9b3813786132c7530df9b9a9a828a0
yevhen <yevhen.krasnokutsky@gmail.com>
2022-09-24 11:05:08 +0300

    W3HW5 model card refactoring

--- week-3/model-card.py
+++ week-3/model-card.py
@@ -13,12 +13,59 @@ import seaborn as sns
 import uuid
 
 
+NAME = 'Yevhen K.'
+CONTACT = 'name@example.com'
+CARD_NAME = 'RandomFores Classifier for MakeMoons Dataset'
+CARD_OVERVIEW = 'Simple Card With RandomForest Classifier for Dummy Data'
+REFERENCES = [
+    'https://www.tensorflow.org/responsible_ai/model_card_toolkit/examples/Scikit_Learn_Model_Card_Toolkit_Demo',
+    'https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html',
+]
+LIMITATIONS = 'Random forest classification of dummy dataset'
+DESCRIPTION = 'Education with classification'
+EXPORT_PATH = 'card.html'
+
+
 # Utility function that will export a plot to a base-64 encoded string that the model card will accept.
 
 def plot_to_str():
     img = BytesIO()
     plt.savefig(img, format='png')
     return base64.encodebytes(img.getvalue()).decode('utf-8')
+
+
+def make_card(X_train, dataset_plot, analysis_plots):
+    ##  Initialize toolkit and model card
+    mct = mctlib.ModelCardToolkit()
+    model_card = mct.scaffold_assets()
+
+    model_card.model_details.name = CARD_NAME
+    model_card.model_details.overview = (CARD_OVERVIEW)
+    model_card.model_details.owners = [
+        mctlib.Owner(name= NAME, contact=CONTACT)
+    ]
+    model_card.model_details.references = [
+        mctlib.Reference(reference=reference) for reference in REFERENCES
+    ]
+    model_card.model_details.version.name = str(uuid.uuid4())
+    model_card.model_details.version.date = str(date.today())
+
+    model_card.considerations.limitations = [mctlib.Limitation(description=LIMITATIONS)]
+    model_card.considerations.use_cases = [mctlib.UseCase(description=DESCRIPTION)]
+
+    model_card.model_parameters.data.append(mctlib.Dataset())
+    model_card.model_parameters.data[0].graphics.description = (
+      f'{len(X_train)} rows with {len(X_train[0])} features')
+    model_card.model_parameters.data[0].graphics.collection = [
+        mctlib.Graphic(image=dataset_plot)
+    ]
+    
+    model_card.quantitative_analysis.graphics.description = (analysis_plots["desc"])
+    model_card.quantitative_analysis.graphics.collection = [
+        mcvlib.Graphic(image=plot) for plot in analysis_plots["plots"]
+    ]
+    mct.update_model_card(model_card)
+    return mct
 
 
 def main():
@@ -47,49 +94,18 @@ def main():
     plot_confusion_matrix(clf, X_test, y_test)
     confusion_matrix = plot_to_str()
 
+    analysis_plots = {
+        "desc": "ROC curve and confusion matrix",
+        "plots": [roc_curve, confusion_matrix],
+    }
+
     ### Create a model card
-    ##  Initialize toolkit and model card
-    mct = mctlib.ModelCardToolkit()
-    model_card = mct.scaffold_assets()
-
-    model_card.model_details.name = 'RandomFores Classifier for MakeMoons Dataset'
-    model_card.model_details.overview = (
-        'Simple Card With RandomForest Classifier for Dummy Data')
-    model_card.model_details.owners = [
-        mctlib.Owner(name= 'Yevhen K.', contact='name@example.com')
-    ]
-    model_card.model_details.references = [
-        mctlib.Reference(reference='https://www.tensorflow.org/responsible_ai/model_card_toolkit/examples/Scikit_Learn_Model_Card_Toolkit_Demo'),
-        mctlib.Reference(reference='https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html')
-    ]
-    model_card.model_details.version.name = str(uuid.uuid4())
-    model_card.model_details.version.date = str(date.today())
-
-    model_card.considerations.limitations = [mctlib.Limitation(description='Random forest classification of dummy dataset')]
-    model_card.considerations.use_cases = [mctlib.UseCase(description='Education with classification')]
-
-    model_card.model_parameters.data.append(mctlib.Dataset())
-    model_card.model_parameters.data[0].graphics.description = (
-      f'{len(X_train)} rows with {len(X_train[0])} features')
-    model_card.model_parameters.data[0].graphics.collection = [
-        mctlib.Graphic(image=dataset_plot)
-    ]
-    
-    model_card.quantitative_analysis.graphics.description = (
-      'ROC curve and confusion matrix')
-    model_card.quantitative_analysis.graphics.collection = [
-        mctlib.Graphic(image=roc_curve),
-        mctlib.Graphic(image=confusion_matrix)
-    ]
-    mct.update_model_card(model_card)
-
+    mct = make_card(X_train, dataset_plot, analysis_plots)
     ### Generate model card
     # Return the model card document as an HTML page
     html = mct.export_format()
-    with open("card.html", "wt") as f:
+    with open(EXPORT_PATH, "wt") as f:
         f.write(html)
-
-
     
 if __name__ == "__main__":
     main()
